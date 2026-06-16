f68944491cb288bb4c08583e58e3c3ab3e46e8b1
Dustin Lee Bayn <96162801+dustinlbayn@users.noreply.github.com>
2026-06-16 11:36:51 -0400

    Include repair command in Debian package

--- extensions/novnc-clipboard-panel/packaging/build-deb.sh
+++ extensions/novnc-clipboard-panel/packaging/build-deb.sh
@@ -11,6 +11,7 @@ OUT_DIR="$ROOT_DIR/packaging/dist"
 rm -rf "$BUILD_ROOT"
 mkdir -p "$PKG_ROOT/DEBIAN"
 mkdir -p "$PKG_ROOT/usr/share/dennco/novnc-clipboard-panel"
+mkdir -p "$PKG_ROOT/usr/sbin"
 mkdir -p "$OUT_DIR"
 
 cp "$ROOT_DIR/packaging/debian/control" "$PKG_ROOT/DEBIAN/control"
@@ -20,6 +21,8 @@ chmod 0755 "$PKG_ROOT/DEBIAN/postinst" "$PKG_ROOT/DEBIAN/prerm"
 
 cp "$ROOT_DIR/src/novnc-clipboard-panel.js" "$PKG_ROOT/usr/share/dennco/novnc-clipboard-panel/novnc-clipboard-panel.js"
 cp "$ROOT_DIR/src/novnc-clipboard-panel.css" "$PKG_ROOT/usr/share/dennco/novnc-clipboard-panel/novnc-clipboard-panel.css"
+cp "$ROOT_DIR/install/repair.sh" "$PKG_ROOT/usr/sbin/dennco-novnc-clipboard-repair"
+chmod 0755 "$PKG_ROOT/usr/sbin/dennco-novnc-clipboard-repair"
 
 dpkg-deb --build "$PKG_ROOT" "$OUT_DIR/${PKG_NAME}_${VERSION}_all.deb"
 
